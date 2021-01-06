7ef68e6363fc80fcf2e6971ae243d4db05138e30
esther8283 <esther8283@naver.com>
2021-01-06 15:46:30 +0900

    ch02 프로젝트

---
+++ src/ch02/ch02.java
@@ -0,0 +1,26 @@
+package ch02;
+
+public class ch02 {
+
+	public static void main(String[] args) {
+		
+		implicitconversion conversion = new
+implicitconversion();
+		conversion.implicitConversion();
+		
+		ExplicitConversion conversion2 = new
+ExplicitConversion();
+		conversion2.explicitConversion();
+		
+		OperationEx1 conversion3 = new
+OperationEx1();
+		conversion3.operationEx1();
+		
+		OperationEx2 conversion4 = new
+OperationEx2();
+		conversion4.operationEx2();
+
+	}
+
+}
+	
